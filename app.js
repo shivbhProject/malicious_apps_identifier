ca3d4244e718bbbec8d8a02e19037100df1fcb09
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2022-07-14 22:02:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var gplay = require('google-play-scraper');
 
 async function search_apps(t) {
-    let abc = await gplay.search({term: t, num: 1, country: 'in', fullDetail: true});
+    let abc = await gplay.search({term: t, num: 10, country: 'in', fullDetail: true});
     return abc;
 }
 
+async function get_permissions(app_id) {
+    let abc = await gplay.app({appId: app_id});
+    return abc.permissions;
+}
+
 async function main() {
+
+    let filter_inr = true;
+    let filter_email = true;
+
     let terms = ["loan", "cash", "app", "rupee", "credit", "money", "paisa", "coin", "wallet", "easy", "pocket"];
     appslist = []
     for (const term of terms){
@@ -14,9 +23,19 @@ async function main() {
     }
     // console.log(appslist.length);
     // console.log(appslist);
+
+    let filtered_list = [];
     for (const app of appslist){
-        console.log(app.developerEmail, app.currency);
+        // console.log(app.developerEmail, app.currency);
+        if(filter_inr && app.currency == "INR"){
+            filtered_list.push(app);
+        }
+        if(filter_email && (app.developerEmail != "" || (app.developerEmail.endsWith("@gmail.com")) || (app.developerEmail.endsWith("@hotmail.com")) || (app.developerEmail.endsWith("@yahoo.com")))){
+            filtered_list.push(app);
+        }
     }
+
+    console.log(filtered_list);
 }
 
 main();
