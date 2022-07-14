600e1d9f7fecdad1342b19ad4307affe0e719019
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2022-07-14 20:06:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 var gplay = require('google-play-scraper');
 
-var list = gplay.app({appId: 'com.google.android.apps.translate'})
-//   .then(console.log, console.log);
+async function search_apps(t) {
+    let abc = await gplay.search({term: t, num: 100, country: 'in'});
+    return abc;
+}
 
-console.log(list);
+async function main() {
+    let terms = ["loan", "cash", "app", "rupee", "credit", "money", "paisa", "coin", "wallet", "easy", "pocket"];
+    appslist = []
+    for (const term of terms){
+        let temp = await search_apps(term);
+        appslist.push(...temp);
+        // console.log(appslist.length);
+    }
+    console.log(appslist);
+}
+
+main();
