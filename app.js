6429d85a2c87d8f19d9900027ce194374abd56f8
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2022-07-14 23:32:43 +0530

    Added permissions filter and found some suspects

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var gplay = require('google-play-scraper');
 
 async function search_apps(t) {
-    let abc = await gplay.search({term: t, num: 10, country: 'in', fullDetail: true});
+    let abc = await gplay.search({ term: t, num: 200, country: 'in', fullDetail: true });
     return abc;
 }
 
-async function get_permissions(app_id) {
-    let abc = await gplay.app({appId: app_id});
-    return abc.permissions;
+async function get_permissions(id) {
+    let abc = await gplay.permissions({ appId: id });
+    return abc;
 }
 
 async function main() {
@@ -17,25 +17,30 @@ async function main() {
 
     let terms = ["loan", "cash", "app", "rupee", "credit", "money", "paisa", "coin", "wallet", "easy", "pocket"];
     appslist = []
-    for (const term of terms){
+    for (const term of terms) {
         let temp = await search_apps(term);
         appslist.push(...temp);
     }
     // console.log(appslist.length);
     // console.log(appslist);
 
+    let allapps = [...new Set(appslist)];
     let filtered_list = [];
-    for (const app of appslist){
-        // console.log(app.developerEmail, app.currency);
-        if(filter_inr && app.currency == "INR"){
-            filtered_list.push(app);
-        }
-        if(filter_email && (app.developerEmail != "" || (app.developerEmail.endsWith("@gmail.com")) || (app.developerEmail.endsWith("@hotmail.com")) || (app.developerEmail.endsWith("@yahoo.com")))){
-            filtered_list.push(app);
+    for (const app of allapps) {
+        if (app.currency == "INR" && (app.developerEmail == "" || (app.developerEmail.endsWith("@gmail.com")) || (app.developerEmail.endsWith("@hotmail.com")) || (app.developerEmail.endsWith("@yahoo.com")))) {
+            
+            let perms = await get_permissions(app.appId);
+            for (const perm of perms) {
+                if(perm.type=="Camera" || perm.type=="Microphone"){
+                    filtered_list.push(app);
+                    console.log(app.title, ", ", app.appId, ", ", app.currency, ", ", app.developerEmail, ", ", perm.type);
+                    break;
+                }
+            }
         }
     }
 
-    console.log(filtered_list);
+    // console.log(filtered_list);
 }
 
 main();
